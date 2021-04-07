8cf38cbb73f69b50329956ecdce11c7fff583bc1
Jakub Darul <daruljakub@protonmail.com>
2021-04-07 23:03:34 +0200

    fix: dependency cleanup

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,21 +1,16 @@
 object Dependencies {
   import sbt._
-  val ScalaTestVersion  = "3.2.3"
-  val ScalaMockVersion  = "5.0.0"
-  val FastParseVersion  = "2.3.0"
-  val CatsVersion       = "2.3.0"
-  val CatsEffectVersion = "2.3.0"
-  val MouseVersion      = "0.26.2"
-  val GuavaVersion      = "30.0-jre"
-  val DiffxVersion      = "0.3.30"
-  val ScalaPbVersion    = "0.11.1"
+  val ScalaTestVersion = "3.2.7"
+  val FastParseVersion = "2.3.2"
+  val CatsVersion      = "2.5.0"
+  val MouseVersion     = "1.0.2"
+  val GuavaVersion     = "30.1.1-jre"
+  val DiffxVersion     = "0.4.5"
 
   val catsDependencies = Seq(
-    "org.typelevel" %% "cats-core"   % CatsVersion,
-    "org.typelevel" %% "cats-effect" % CatsEffectVersion,
-    "org.typelevel" %% "mouse"       % MouseVersion,
+    "org.typelevel" %% "cats-core" % CatsVersion,
+    "org.typelevel" %% "mouse"     % MouseVersion,
     // "org.scalameta"        %% "scalameta"       % "4.4.11",
-    "com.lihaoyi"   %% "pprint"      % "0.5.7",
   )
 
   private val miscDependencies = Seq(
@@ -25,7 +20,6 @@ object Dependencies {
 
   private val testDependencies = Seq(
     "org.scalatest"          %% "scalatest"       % ScalaTestVersion % Test,
-    "org.scalamock"          %% "scalamock"       % ScalaMockVersion % Test,
     "com.softwaremill.diffx" %% "diffx-scalatest" % DiffxVersion     % Test,
   )
 
