8204339e5989f3201facdca1e84f9d0684574d47
Scala Steward <scala-steward@example.com>
2022-04-03 01:01:53 +0000

    Update pprint to 0.7.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     "com.google.guava" % "guava"      % GuavaVersion,
     "org.scalameta"   %% "scalameta"  % ScalametaVersion cross CrossVersion.for3Use2_13,
     "com.lihaoyi"     %% "ujson"      % "1.5.0" cross CrossVersion.for3Use2_13,
-    "com.lihaoyi"     %% "pprint"     % "0.7.2" cross CrossVersion.for3Use2_13,
+    "com.lihaoyi"     %% "pprint"     % "0.7.3" cross CrossVersion.for3Use2_13,
   )
 
   private val testDependencies = Seq(
