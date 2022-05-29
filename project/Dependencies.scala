b067792f0f59db0dba8eff9af36caabf67f80f30
Scala Steward <scala-steward@example.com>
2022-05-29 00:13:49 +0000

    Update scalameta to 4.5.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val CatsVersion      = "2.7.0"
   val MouseVersion     = "1.0.11"
   val GuavaVersion     = "31.1-jre"
-  val ScalametaVersion = "4.5.6"
+  val ScalametaVersion = "4.5.8"
 
   val catsDependencies = Seq(
     "org.typelevel" %% "cats-core" % CatsVersion,
