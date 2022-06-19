6972c2607d59a90356df3cac6fa82f8bbf778030
Scala Steward <scala-steward@example.com>
2022-06-19 00:12:58 +0000

    Update cats-core to 2.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 object Dependencies {
   import sbt._
   val CatsParseVersion = "0.3.7"
-  val CatsVersion      = "2.7.0"
+  val CatsVersion      = "2.8.0"
   val MouseVersion     = "1.0.11"
   val GuavaVersion     = "31.1-jre"
   val ScalametaVersion = "4.5.6"
