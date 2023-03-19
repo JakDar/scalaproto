204d5fd1c0036291ae08013808ce572accfbdfa7
Scala Steward <scala-steward@example.com>
2023-03-19 00:13:26 +0000

    Update ujson to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     "org.typelevel"   %% "cats-parse" % CatsParseVersion,
     "com.google.guava" % "guava"      % GuavaVersion,
     "org.scalameta"   %% "scalameta"  % ScalametaVersion cross CrossVersion.for3Use2_13,
-    "com.lihaoyi"     %% "ujson"      % "2.0.0" cross CrossVersion.for3Use2_13,
+    "com.lihaoyi"     %% "ujson"      % "3.0.0" cross CrossVersion.for3Use2_13,
     "com.lihaoyi"     %% "pprint"     % "0.7.3" cross CrossVersion.for3Use2_13,
   )
 
