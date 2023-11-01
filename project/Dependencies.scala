6be77ae0526259ec1c4a7632fedce1a0e4c08483
Jakub Darul <daruljakub@protonmail.com>
2023-11-01 12:05:03 +0100

    bump deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,10 @@
 object Dependencies {
   import sbt._
   val CatsParseVersion = "0.3.9"
-  val CatsVersion      = "2.9.0"
+  val CatsVersion      = "2.10.0"
   val MouseVersion     = "1.2.1"
   val GuavaVersion     = "31.1-jre"
-  val ScalametaVersion = "4.8.5"
+  val ScalametaVersion = "4.8.12"
 
   val catsDependencies = Seq(
     "org.typelevel" %% "cats-core" % CatsVersion,
@@ -15,12 +15,12 @@ object Dependencies {
     "org.typelevel"   %% "cats-parse" % CatsParseVersion,
     "com.google.guava" % "guava"      % GuavaVersion,
     "org.scalameta"   %% "scalameta"  % ScalametaVersion cross CrossVersion.for3Use2_13,
-    "com.lihaoyi"     %% "ujson"      % "3.1.2" cross CrossVersion.for3Use2_13,
+    "com.lihaoyi"     %% "ujson"      % "3.1.3" cross CrossVersion.for3Use2_13,
     "com.lihaoyi"     %% "pprint"     % "0.8.1" cross CrossVersion.for3Use2_13,
   )
 
   private val testDependencies = Seq(
-    "org.scalameta" %% "munit" % "1.0.0-M8" % Test
+    "org.scalameta" %% "munit" % "1.0.0-M10" % Test
   )
 
   val all: Seq[ModuleID] = Seq(
